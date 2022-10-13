8af739e8ac709252d6a6dbb0c1321800d42221f0
aris cruz <ariscruz.va@gmail.com>
2022-10-13 20:52:24 +0800

    test-activity-sample-ui

---
+++ app/src/main/java/com/kodego/inventory/app/cruz/TestActivity.kt
@@ -0,0 +1,68 @@
+package com.kodego.inventory.app.cruz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.kodego.inventory.app.cruz.databinding.ActivityTestBinding
+
+class TestActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityTestBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+                //button,editext and textview
+        binding.btnGetName.setOnClickListener(){
+//            Toast.makeText(applicationContext, "Enter Your Name", Toast.LENGTH_LONG).show()
+            var name : String = binding.etvName.text.toString()
+            binding.txtName.text = name
+
+            binding.btnGetName.setOnClickListener(){
+                var name : String = binding.etvName.text.toString()
+                binding.txtName.text = name
+
+                var spinnerItem:String = binding.spinnerId.selectedItem.toString()
+                Toast.makeText(applicationContext,spinnerItem,Toast.LENGTH_SHORT).show()
+            }
+        }
+        //radiobutton and radiogroup
+        binding.radioGroup.setOnCheckedChangeListener { radioGroup, checkedOption ->
+            when(checkedOption){
+                R.id.rb1 -> Toast.makeText(applicationContext, "Option 1 Selected", Toast.LENGTH_LONG).show()
+                R.id.rb2 -> Toast.makeText(applicationContext, "Option 2 Selected", Toast.LENGTH_SHORT).show()
+                R.id.rb3 -> Toast.makeText(applicationContext, "Option 3 Selected", Toast.LENGTH_LONG).show()
+            }
+        }
+        //checkbox
+        binding.cb1.setOnClickListener(){
+            if (binding.cb1.isChecked){
+                Toast.makeText(applicationContext,"Checkbox 1 is checked",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext,"Checkbox 1 is unchecked",Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.cb2.setOnClickListener() {
+            if (binding.cb2.isChecked) {
+                Toast.makeText(applicationContext, "Checkbox 2 is checked", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "Checkbox 2 is unchecked", Toast.LENGTH_SHORT).show()
+            }
+        }
+        //spinner
+        val data = arrayListOf<String>("Option 1","Option 2","Option 3")
+        val adapterParent = ArrayAdapter(applicationContext, R.layout.textview_xml,data)
+
+        binding.spinnerId.adapter = adapterParent
+
+        binding.switchButton.setOnClickListener(){
+            if (binding.switchButton.isChecked){
+                Toast.makeText(applicationContext,"ON",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext,"OFF",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
