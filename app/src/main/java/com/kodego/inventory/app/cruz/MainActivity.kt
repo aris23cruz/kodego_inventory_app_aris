d5fea4bf2c671234a97e40ba3fcee078dd9fca00
aris cruz <ariscruz.va@gmail.com>
2022-10-14 20:24:32 +0800

    add login function and homescreen

--- app/src/main/java/com/kodego/inventory/app/cruz/MainActivity.kt
+++ app/src/main/java/com/kodego/inventory/app/cruz/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.kodego.inventory.app.cruz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.kodego.inventory.app.cruz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //login
+        binding.btnLogin.setOnClickListener (){
+            var userName : String = binding.etvUserName.text.toString()
+            var password : String = binding.etvPassword.text.toString()
+            checkCredential(userName,password)
+        }
+    }
+    private fun checkCredential(userName:String, password:String): Boolean{
+        val correctUserName:String = "admin"
+        val correctPassword:String = "admin123"
+
+        if((correctUserName == userName)&&(correctPassword == password)){
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(applicationContext,"Logging in....", Toast.LENGTH_SHORT).show()
+            finish()
+            return true
+        }else{
+            Toast.makeText(applicationContext,"Invalid Credentials", Toast.LENGTH_SHORT).show()
+            return false
+        }
     }
 }
